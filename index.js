2209af7582927271053f121337efb99466da186c
recepduran <98320493+recepduran@users.noreply.github.com>
2022-08-11 10:34:30 +0300

    updated with citylist json

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// run `node index.js` in the terminal
-
-const express = require('express');
-
+const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 
-app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('merhabalar olsun');
-});
+const router = express.Router();
 
-app.listen(process.env.PORT, () => {
-  console.log('started nodu');
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var cities = [{name: 'Istanbul', country: 'Turkey'}, {name: 'New York', country: 'USA'}, {name: 'London', country:'England'}];
+
+app.use("/", router);
+router.get("/", (req,res)=>{
+  res.status(200).setHeader('Content-Type', 'application/json').json(cities)
+  return json(cities)
+})
+
+
+app.listen(3000, () => {
+  console.log('server started');
 });
